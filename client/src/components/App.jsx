bd9773196ded5b95f36835318a8e7fa14f7be241
Jon Baltz <baltz.jon.r@gmail.com>
2020-04-18 16:10:53 -0600

    Completed hiding and revealing function

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,25 +6,29 @@ class App extends Component {
 		this.state = {
 			file: null,
 			alert: '',
-			message: 'Enter Message Here'
+			message: 'Enter Message Here',
+			decoded: ''
 		}
 		this.handleImageChange = this.handleImageChange.bind(this);
 		this.handleMessageChange = this.handleMessageChange.bind(this);
 		this.hide = this.hide.bind(this);
 		this.reveal = this.reveal.bind(this);
+		this.padString = this.padString.bind(this);
+		this.string2Binary = this.string2Binary.bind(this);
+		this.binary2String = this.binary2String.bind(this);
 	}
 	handleImageChange(event) {
 		const image = event.target.files[0];
-		if (image.type === "image/png") {
-			this.setState({
-				file: URL.createObjectURL(event.target.files[0])
-			});
-		} else {
-			this.setState({
-				file: null,
-				alert: 'Please input a png image'
-			});
-		}
+		// if (image.type === "image/jpg") {
+		this.setState({
+			file: URL.createObjectURL(event.target.files[0])
+		});
+		// } else {
+		// 	this.setState({
+		// 		file: null,
+		// 		alert: 'Please input a png or jpg image'
+		// 	});
+		// }
 	}
 	handleMessageChange(event) {
 		console.log(event.target.value);
@@ -33,17 +37,64 @@ class App extends Component {
 		});
 	}
 
+	padString(n, w = 7) {
+		return n.length >= w ? n : new Array(w - n.length + 1).join('0') + n;
+	}
+
+	string2Binary(string) {
+		const that = this;
+		return string.split('').map(function (char) {
+			return that.padString(char.charCodeAt(0).toString(2));
+		}).join(' ');
+	}
+
+	binary2String(binary) {
+		return binary.split(' ').map(function (val) {
+			return String.fromCharCode(parseInt(val, 2));
+		}).join('');
+	}
+
 	hide() {
-		var c = document.getElementById('altered');
-		var context = c.getContext("2d");
-		var img = document.getElementById('original');
+		const c = document.getElementById('altered');
+		const context = c.getContext("2d");
+		const img = document.getElementById('original');
 		context.drawImage(img, 0, 0);
-		var imageData = context.getImageData(0, 0, img.width, img.height);
-		var data = imageData.data;
+		const imageData = context.getImageData(0, 0, img.width, img.height);
+		const data = imageData.data;
+		const message = this.string2Binary(this.state.message).split(' ').join('');
+		let count = 0;
+		for (var i = 0; i < data.length; i += 4) {
+			if (data[i + 2] < 5 && count < message.length) {
+				data[i + 2] = message[count];
+				count++;
+			}
+		}
+		context.putImageData(imageData, 0, 0);
 	}
 
 	reveal() {
-		console.log('revealed');
+		const c = document.getElementById('altered');
+		const context = c.getContext("2d");
+		const img = document.getElementById('original');
+		context.drawImage(img, 0, 0);
+		const imageData = context.getImageData(0, 0, img.width, img.height);
+		const data = imageData.data;
+		let decoded = '';
+		let space = 0;
+		for (var i = 0; i < data.length; i += 4) {
+			if (data[i + 2] === 1 || data[i + 2] === 0) {
+				if (space === 7) {
+					decoded += ' ';
+					space = 0;
+				}
+				decoded += data[i + 2];
+				space++;
+			}
+		}
+		decoded = this.binary2String(decoded);
+		this.setState({
+			decoded
+		});
 	}
 
 	render() {
@@ -57,8 +108,9 @@ class App extends Component {
 				<button onClick={this.hide}>Hide</button>
 				<button onClick={this.reveal}>Reveal</button>
 				<textarea id="message" onChange={this.handleMessageChange} rows="4" cols="50" defaultValue="Enter Message Here"></textarea>
-				<img id="original" src={this.state.file} width="220" height="230" />
-				<canvas id="altered" width="220" height="230">Your Browser doesn't support canvas</canvas>
+				<img id="original" src={this.state.file} width="220" height="277" />
+				<canvas id="altered" width="220" height="277">Your Browser doesn't support canvas</canvas>
+				<p className="decoded">{this.state.decoded}</p>
 			</div>
 		);
 	}
--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const Passwords = require('./database/passwords.js');
+
 const port = 9000;
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/authentication', (req, res) => {
+	res.send('hello');
+});
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
