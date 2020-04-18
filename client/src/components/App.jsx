6c679f71bb442262df5e18548d07774c7f5f9187
Jon Baltz <baltz.jon.r@gmail.com>
2020-04-18 12:54:30 -0600

    Added image display and canvas create

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const App = () => {
-  return <div>Hello World</div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			file: null,
+			alert: '',
+			message: 'Enter Message Here'
+		}
+		this.handleImageChange = this.handleImageChange.bind(this);
+		this.handleMessageChange = this.handleMessageChange.bind(this);
+		this.hide = this.hide.bind(this);
+	}
+	handleImageChange(event) {
+		const image = event.target.files[0];
+		if (image.type === "image/png") {
+			this.setState({
+				file: URL.createObjectURL(event.target.files[0])
+			});
+		} else {
+			this.setState({
+				file: null,
+				alert: 'Please input a png image'
+			});
+		}
+	}
+	handleMessageChange(event) {
+		console.log(event.target.value);
+		this.setState({
+			message: event.target.value
+		});
+	}
+
+	hide() {
+		var c = document.getElementById('altered');
+		var context = c.getContext("2d");
+		var img = document.getElementById('original');
+		context.drawImage(img, 0, 0);
+		var imageData = context.getImageData(0, 0, img.width, img.height);
+		var data = imageData.data;
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Steganopgraphy</h1>
+				<p>Explainer Paeragraph</p>
+				<label htmlFor="file" accept="image/*, .png">Insert a .png file </label>
+				<input onChange={this.handleImageChange} type="file"></input>
+				<p className="alert">{this.state.alert}</p>
+				<button onClick={this.hide}>Hide</button>
+				<textarea id="message" onChange={this.handleMessageChange} rows="4" cols="50" defaultValue="Enter Message Here"></textarea>
+				<img id="original" src={this.state.file} width="220" height="230" />
+				<canvas id="altered" width="220" height="230">Your Browser doesn't support canvas</canvas>
+			</div>
+		);
+	}
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname, '/client/src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'client/dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-              test: /\.(js|jsx)$/,
-                exclude: '/node-modules/',
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    }
+	entry: path.join(__dirname, '/client/src/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'client/dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: '/node-modules/',
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
 };
